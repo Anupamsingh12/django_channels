9983d0975ab3e97c9d6bf102919f953516863a5b
Anupamsingh12 <anupmsingh12@gmail.com>
2021-05-23 11:20:18 +0530

    commited

---
+++ chat/routing.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter,URLRouter
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+]
